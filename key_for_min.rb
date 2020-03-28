e0bf1743bff175ac6679f0824f56e408b6aeb5f9
Micah McCallum <micahmccallum@outlook.com>
2020-03-28 04:09:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,34 +1,30 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+# def key_for_min_value(name_hash)
+#   if name_hash.length == 0
+#     return nil
+#   end
+#   nameHashValues = []
+#   nameHashValues = name_hash.to_a
+#   i = nameHashValues.length
+#   while i > 1
+#     if nameHashValues.first[1] < nameHashValues.last[1]
+#       nameHashValues.pop
+#     else
+#       nameHashValues.shift
+#     end
+#     i -= 1
+#   end
+#   name = (name_hash.select { |key, value| value == nameHashValues.first[1] }).to_a
+#   name[0][0]
+# end
 
-def key_for_min_value(name_hash)
-  if name_hash.length == 0
-    return nil
-  end
-  nameHashValues = []
-  nameHashValues = name_hash.to_a
-  i = nameHashValues.length
-  while i > 1
-    if nameHashValues.first[1] < nameHashValues.last[1]
-      nameHashValues.pop
-    else
-      nameHashValues.shift
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
     end
-    i -= 1
   end
-  name = (name_hash.select { |key, value| value == nameHashValues.first[1] }).to_a
-  name[0][0]
+  lowest_key
 end
-
-  #   value_a = nameHashValues[ i - 1 ]
-  #   value_b = nameHashValues[ i - 2 ]
-  #   if value_a > value_b
-  #     nameHashValues.pop
-  #   else
-  #     nameHashValues.delete[ i - 2 ]
-  #   end
-  #   i -= 1
-  # end
-  # name_hash.select do |key, value|
-  #   value == nameHashValues[0]
-  #   return key
